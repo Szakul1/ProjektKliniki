cbd1cc1f0eceb90a0fce19e8b6abeb9de48b11f0
Szakul1 <56487624+Szakul1@users.noreply.github.com>
2019-12-20 17:32:15 +0100

    example

--- clientPanel.java
+++ clientPanel.java
@@ -1,15 +1,18 @@
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class clientPanel extends myPanel
+public class clientPanel extends JPanel
 {
     /**
      *
      */
     private static final long serialVersionUID = 1L;
 
+    myPanel select;
     public clientPanel(JFrame frame) 
     {
-        super(new String[]{"id","imię","nazwisko","numer_tel","zniżka%"},frame);
+        select = new myPanel(new String[]{"id","imię","nazwisko","numer_tel","zniżka%"},frame);
+        add(select);
     }
 
     
