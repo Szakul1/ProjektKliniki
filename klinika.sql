b808931130632c28853d8a3c7b4ac5c8ab90f480
Szakul1 <56487624+Szakul1@users.noreply.github.com>
2020-01-10 22:40:12 +0100

    ta wersja jest dobra mam nadzieje

---
+++ klinika.sql
@@ -0,0 +1,108 @@
+-- Created by Vertabelo (http://vertabelo.com)
+-- Last modification date: 2019-12-20 21:41:21.672
+
+-- tables
+-- Table: grafik
+CREATE TABLE grafik (
+    id_pracownika int NOT NULL,
+    dzień enum('poniedziałek','wtorek','środa','czwartek','piątek') NOT NULL,
+    rozpoczęcie time NOT NULL,
+    zakończenie time NOT NULL
+) COMMENT 'przechowuje godziny pracy pracowników';
+
+
+-- Table: klienci
+CREATE TABLE klienci (
+    id int NOT NULL AUTO_INCREMENT,
+    imię varchar(30) NOT NULL,
+    nazwisko varchar(30) NOT NULL,
+    numer_tel int NOT NULL,
+    CONSTRAINT klienci_pk PRIMARY KEY (id)
+) COMMENT 'przechowuje dane klientów w klinice';
+
+-- Table: pracownicy
+CREATE TABLE pracownicy (
+    id int NOT NULL AUTO_INCREMENT,
+    imię varchar(30) NOT NULL,
+    nazwisko varchar(30) NOT NULL,
+    numer_tel int NOT NULL,
+    data_urodzenia date NOT NULL,
+    pensja int NOT NULL CHECK (pensja>=0),
+    zawód enum('weterynarz','technik') NOT NULL,
+    CONSTRAINT pracownicy_pk PRIMARY KEY (id)
+) COMMENT 'przechowuje dane pracowników pracujących w klinice';
+
+-- Table: szczepienia
+CREATE TABLE szczepienia (
+    id_zwierzęcia int NOT NULL,
+    choroba int NOT NULL,
+    czy_w_tej_klinice bool NOT NULL
+);
+
+-- Table: usługi
+CREATE TABLE usługi (
+    nazwa varchar(30) NOT NULL,
+    cena int NOT NULL CHECK (cena>=0),
+    zawód enum('weterynarz','technik') NOT NULL COMMENT 'kto może wykonać daną usługę',
+    CONSTRAINT usługi_pk PRIMARY KEY (nazwa)
+) COMMENT 'przechowuje możliwe do wykonania usługi w klinice';
+
+-- Table: użytkownicy
+CREATE TABLE użytkownicy (
+    login varchar(30) NOT NULL,
+    hasło varchar(32) NOT NULL,
+    uprawnienia enum('klient','technik','weterynarz','dyrektor','admin') NOT NULL COMMENT 'stopień dostępu do bazy danych',
+    id int NULL COMMENT 'id pracownika lub klienta',
+    CONSTRAINT użytkownicy_pk PRIMARY KEY (login)
+) COMMENT 'przechowuje dane niezbędne do logowania';
+
+-- Table: wizyty
+CREATE TABLE wizyty (
+    id_pracownika int NOT NULL,
+    id_zwierzęcia int NOT NULL,
+    termin timestamp NOT NULL CHECK (TIME(termin) BETWEEN '7:00' AND '17:00'),
+    cel_wizyty varchar(30) NOT NULL,
+    opłata int NOT NULL CHECK (opłata>=0)
+) COMMENT 'tabela przechowująca odbyte oraz zaplanowane wizyty';
+
+-- Table: zwierzęta
+CREATE TABLE zwierzęta (
+    id_zwierzęcia int NOT NULL AUTO_INCREMENT,
+    id_klienta int NOT NULL,
+    imię varchar(30) NOT NULL,
+    data_urodzenia date NOT NULL,
+    gatunek varchar(30) NOT NULL,
+    waga int NOT NULL,
+    płeć enum('f','m') NOT NULL,
+    kastrowane bool NOT NULL,
+    kraj_pochodzenia varchar(30) NOT NULL,
+    CONSTRAINT zwierzęta_pk PRIMARY KEY (id_zwierzęcia)
+) COMMENT 'tabela łącząca zwierzęta z klientami kliniki';
+
+-- foreign keys
+-- Reference: grafik_pracownicy (table: grafik)
+ALTER TABLE grafik ADD CONSTRAINT grafik_pracownicy FOREIGN KEY grafik_pracownicy (id_pracownika)
+    REFERENCES pracownicy (id);
+
+-- Reference: pracownik_wizyta (table: wizyty)
+ALTER TABLE wizyty ADD CONSTRAINT pracownik_wizyta FOREIGN KEY pracownik_wizyta (id_pracownika)
+    REFERENCES pracownicy (id);
+
+-- Reference: wizyta_usługa (table: wizyty)
+ALTER TABLE wizyty ADD CONSTRAINT wizyta_usługa FOREIGN KEY wizyta_usługa (cel_wizyty)
+    REFERENCES usługi (nazwa);
+
+-- Reference: zwierzę_klienci (table: zwierzęta)
+ALTER TABLE zwierzęta ADD CONSTRAINT zwierzę_klienci FOREIGN KEY zwierzę_klienci (id_klienta)
+    REFERENCES klienci (id);
+
+-- Reference: zwierzę_wizyta (table: wizyty)
+ALTER TABLE wizyty ADD CONSTRAINT zwierzę_wizyta FOREIGN KEY zwierzę_wizyta (id_zwierzęcia)
+    REFERENCES zwierzęta (id_zwierzęcia);
+
+-- Reference: zwierzęta_szczepienia (table: szczepienia)
+ALTER TABLE szczepienia ADD CONSTRAINT zwierzęta_szczepienia FOREIGN KEY zwierzęta_szczepienia (id_zwierzęcia)
+    REFERENCES zwierzęta (id_zwierzęcia);
+
+-- End of file.
+
