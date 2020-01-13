fde3e104e6aa52898f324a5fd99985aebb1cf358
KulSmuf <kepka.lukazs@gmail.com>
2020-01-13 16:01:29 +0100

    ss

---
+++ app/panele/servicePanel.java
@@ -0,0 +1,47 @@
+package app;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class servicePanel extends JPanel implements ActionListener
+{
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    myPanel select;
+    JButton add;
+    mainFrame frame;
+
+    public servicePanel(mainFrame frame, Permission perm) 
+    {
+        this.frame = frame;
+        Function fun;
+
+        setLayout(new BorderLayout());
+        add = new JButton("Dodaj Usluge");
+        add.addActionListener(this);
+        if(perm != Permission.TECHNICIAN && perm != Permission.CLIENT)
+        {   
+            fun = Function.DELETE;
+            fun.setId(-1);
+            add(new JPanel().add(add), BorderLayout.EAST);
+        }
+        else
+        {
+            fun = Function.SELECT;
+            fun.setId(perm.getId());
+        }
+        select = new myPanel("uslugi",new String[]{"nazwa","cena","zawod"},frame, fun);
+        add(select, BorderLayout.CENTER);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        if(e.getActionCommand().equals("Dodaj Usluge"))
+            new AddClient(frame);
+    }
+}
