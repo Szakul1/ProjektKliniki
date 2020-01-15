359335badd4f0a8a95e3fb016ed94bd2ddfad398
Szakul1 <56487624+Szakul1@users.noreply.github.com>
2020-01-15 14:53:35 +0100

    masz

---
+++ mainFrame.java
@@ -0,0 +1,82 @@
+package app;
+import app.panele.*;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class mainFrame extends JFrame
+{
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    JTabbedPane cards;
+    JButton myDataJButton;
+    private static final String admin = "admin";
+    private static final String employees = "pracownicy";
+    private static final String services = "uslugi";
+    private static final String clients = "klienci";
+    private static final String animals = "zwierzeta";
+    private static final String visits = "wizyty";
+    private static final String grafic = "grafik";
+    private static final String vaccinations = "szczepienia";
+    private static final String myData = "informacje";
+    DataBaseConnection dataBase;
+    Permision perm;
+    public mainFrame(Permision perm)
+    {
+        this.perm = perm;
+        //perm.setId(1);
+        dataBase = new DataBaseConnection(this);
+        //myDataJButton = new JButton(myData);
+
+        cards = new JTabbedPane();
+        addCards(this.perm);
+
+        setLayout(new BorderLayout());
+        add(cards, BorderLayout.NORTH);
+        pack();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public DataBaseConnection getDataBase()
+    {
+        return dataBase;
+    }
+
+    private void addCards(Permision permissions)
+    {
+        switch (permissions) {
+            case ADMIN:
+                cards.addTab(admin, new adminPanel(this));
+                
+            case DIRECTOR:
+                cards.addTab(employees,new employeePanel(this, permissions));
+
+            case VET:
+                cards.addTab(clients, new clientPanel(this, permissions));
+                
+            case TECHNICIAN:
+                cards.addTab(vaccinations, new vaccinationsPanel(this, permissions));
+
+            case CLIENT:
+            	cards.addTab(services, new servicePanel(this, permissions));
+                cards.addTab(visits, new visitPanel(this, permissions));
+                cards.addTab(animals, new petPanel(this, permissions));
+                cards.addTab(grafic, new graficPanel(this,permissions));
+                //cards.addTab(myData, null);
+                //cards.add(myDataJButton);
+            default:
+                break;
+        }
+        if(permissions==Permision.VET || permissions==Permision.CLIENT || permissions==Permision.TECHNICIAN) {
+        	cards.addTab(myData, new Info(this.getDataBase().select_info(permissions)));
+        }
+    }
+    public static void main(String[] args) {
+        new mainFrame(Permision.ADMIN);
+    }
+}
