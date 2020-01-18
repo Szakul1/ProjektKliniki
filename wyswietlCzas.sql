2cd76d5a4e2c79de6f2b7c6971d52e3360bf7e1f
Szakul1 <56487624+Szakul1@users.noreply.github.com>
2020-01-18 17:02:06 +0100

    Add files via upload

--- wyswietlCzas.sql
+++ wyswietlCzas.sql
@@ -1,6 +1,6 @@
 DROP PROCEDURE IF EXISTS wyswietlCzas;
 delimiter $$
-create procedure wyswietlCzas(in idpracownika int,in terminpod date)
+create procedure wyswietlCzas(in idpracownika int,in terminpod date, in pracownik boolean)
 procedura: begin
 DECLARE rozpoczecievar,zakonczenievar time;
 DECLARE dziennazwa varchar(20);
@@ -17,7 +17,9 @@ WITH RECURSIVE cte (t) AS
 	UNION 
 	SELECT ADDTIME(t,"00:20:00") FROM cte WHERE ADDTIME(t,"00:20:00") <= zakonczenievar
 )
-SELECT * FROM cte WHERE t NOT IN (SELECT TIME(termin) FROM wizyty WHERE DAY(termin)=terminpod AND TIME(termin) BETWEEN rozpoczecievar AND zakonczenievar);
+SELECT * FROM cte WHERE t NOT IN (SELECT TIME(termin) FROM wizyty 
+	WHERE DATE(termin)=terminpod AND TIME(termin) BETWEEN rozpoczecievar AND zakonczenievar) AND (pracownik OR NOT EXISTS
+    (SELECT COUNT(id_zwierzecia) FROM wizyty WHERE DATE(termin) BETWEEN NOW() AND DATE_ADD(NOW(), INTERVAL 1 WEEK)));
 SET @@lc_time_names = 'en_US';
 end $$
 delimiter ;
