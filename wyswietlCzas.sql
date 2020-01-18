75e8e0877a881dbb3d727ab22c874a62e2373967
Szakul1 <56487624+Szakul1@users.noreply.github.com>
2020-01-18 04:03:13 +0100

    blagam ssciagnij to szmato

--- wyswietlCzas.sql
+++ wyswietlCzas.sql
@@ -2,18 +2,22 @@ DROP PROCEDURE IF EXISTS wyswietlCzas;
 delimiter $$
 create procedure wyswietlCzas(in idpracownika int,in terminpod date)
 procedura: begin
-DECLARE rozpoczecie,zakonczenie time;
+DECLARE rozpoczecievar,zakonczenievar time;
+DECLARE dziennazwa varchar(20);
 SET @@lc_time_names = 'pl_PL';
-SET rozpoczecie = (SELECT rozpoczecie FROM grafik WHERE id_pracownika=idpracownika AND dzien=(SELECT DAYNAME(terminpod) dayname));
-SET zakonczenie = (SELECT zakonczenie FROM grafik WHERE id_pracownika=idpracownika AND dzien=(SELECT DAYNAME(terminpod) dayname));
-SET @zajete := (SELECT TIME(termin) FROM wizyty WHERE termin=terminpod AND TIME(termin) BETWEEN rozpoczecie AND zakonczenie);
+SET dziennazwa = (SELECT DAYNAME(terminpod));
+SET dziennazwa = REPLACE(dziennazwa, 'ą', 'a');
+SET dziennazwa = REPLACE(dziennazwa, 'ł', 'l');
+SET dziennazwa = REPLACE(dziennazwa, 'ś', 's');
+SET rozpoczecievar = (SELECT rozpoczecie FROM grafik WHERE id_pracownika=idpracownika AND dzien=dziennazwa);
+SET zakonczenievar = (SELECT zakonczenie FROM grafik WHERE id_pracownika=idpracownika AND dzien=dziennazwa);
 WITH RECURSIVE cte (t) AS
 (
-	SELECT rozpoczecie 
+	SELECT rozpoczecievar 
 	UNION 
-	SELECT ADDTIME(t,"20:00") FROM cte WHERE rozpoczecie <= zakonczenie
+	SELECT ADDTIME(t,"00:20:00") FROM cte WHERE ADDTIME(t,"00:20:00") <= zakonczenievar
 )
-SELECT * FROM cte WHERE t NOT IN (@zajete);
+SELECT * FROM cte WHERE t NOT IN (SELECT TIME(termin) FROM wizyty WHERE DAY(termin)=terminpod AND TIME(termin) BETWEEN rozpoczecievar AND zakonczenievar);
 SET @@lc_time_names = 'en_US';
 end $$
 delimiter ;
